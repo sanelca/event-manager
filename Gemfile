667059992f35e0894f7d4ecb1fa7c706f120b236
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-09-13 15:52:19 +0200

    Hello React

--- Gemfile
+++ Gemfile
@@ -26,7 +26,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'responders'
 
+gem 'webpacker', '>= 4.0.x'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'site#index'
+
+  namespace :api do
+    resources :events, only: %i[index show create destroy update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
