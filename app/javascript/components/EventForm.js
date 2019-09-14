54b8826b272b0d5243b3849e2a5b58e6a0451992
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-09-14 12:38:28 +0200

    Search Implemented

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Pikaday from 'pikaday';
 import 'pikaday/css/pikaday.css';
 import { formatDate, isEmptyObject, validateEvent } from '../helpers/helpers';
+import { Link } from 'react-router-dom';
 
 class EventForm extends React.Component {
   constructor(props) {
@@ -115,10 +116,12 @@ class EventForm extends React.Component {
 
   render() {
     const { event } = this.state;
+    const cancelURL = event.id ? `/events/${event.id}` : '/events';
+    const title = event.id ? `${event.event_date} - ${event.event_type}` : 'New Event';
 
     return (
       <div>
-        <h2>New Event</h2>
+        <h2>{title}</h2>
         {this.renderErrors()}
         <form className="eventForm" onSubmit={this.handleSubmit}>
           <div>
@@ -198,6 +201,7 @@ class EventForm extends React.Component {
           </div>
           <div className="form-actions">
             <button type="submit">Save</button>
+            <Link to={cancelURL}>Cancel</Link>
           </div>
         </form>
       </div>
